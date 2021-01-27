429c0fac531982ac8fa3de9750d10bfef6dfd2cf
narinn-star <skfls2618@naver.com>
2021-01-27 22:27:12 +0900

    21.01.27

---
+++ Problems/Chap02/연습문제07.py
@@ -0,0 +1,4 @@
+l=['Life', 'is', 'too', 'short']
+m = ' '.join(l)
+print(m)
+#join을 사용하면 리스트를 문자열로 쉽게 바꿀 수 있음
---
+++ Problems/Chap02/연습문제10.py
@@ -0,0 +1,5 @@
+a = dict()
+a = {'A':90,'B':80,'C':70}
+b = a.pop('C')
+print(b)
+print(a)
