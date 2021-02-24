bd30f23b4ba1884665fc50ad671c1711b19758fa
narinn-star <skfls2618@naver.com>
2021-02-25 02:13:31 +0900

    21.02.25

---
+++ Nomad 2weeks/Day3/for in.py
@@ -0,0 +1,13 @@
+days = ("Mon", "Tue", "Wed", "Thu", "Fri")
+
+for day in [1,2,3,4,5]:
+    print(day)
+
+for a in days:
+    if a is "Wed":
+        break
+    else:
+        print(a)
+
+for letter in "narin":
+    print(letter)
