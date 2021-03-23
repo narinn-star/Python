8e5a2e853c6c83e64a6ec056f189ce6d570db48a
narinn-star <skfls2618@naver.com>
2021-03-23 23:27:55 +0900

    21.03.23

---
+++ Review/Chapter02/2-09.py
@@ -0,0 +1,23 @@
+#2-09(a)
+
+a = False + False
+print(a, type(a))
+# 0, type : int
+
+#2-09(b)
+
+b = 2 * 3**2.0
+print(b, type(b))
+# 18.0, type : float
+
+#2-09(c)
+
+c = 4 // 2 + 4 % 2
+print(c, type(c))
+# 2, type : int
+
+#2-09(d)
+
+d = 2 + 3 == 4 or 5 >= 5
+print(d, type(d))
+#True, type : bool
