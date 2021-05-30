61fd233fc345485f8a0af61c3b3767c99c61f450
narinn-star <skfls2618@naver.com>
2021-05-30 23:15:42 +0900

    21.05.30

--- Review/Chapter09/Book/click_init.py
+++ Review/Chapter09/Book/click_init.py
@@ -6,4 +6,14 @@ class ClickIt(Frame):
     def __init__(self, master):
         Frame.__init__(self, master)
         self.pack()
-        
+        button = Button(self, text='Click it', command=self.clicked)
+        button.pack()
+
+    def clicked(self):
+        time = strftime('Day : %d %b %Y\nTime: %H:%M:%S %p\n', localtime())
+        showinfo(message=time)
+
+top = Tk()
+app = ClickIt(top)
+app.pack()
+top.mainloop()
